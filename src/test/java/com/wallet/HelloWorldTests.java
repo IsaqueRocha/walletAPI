76b1ea0ece027ef83323620c0c4bb3ecdf1e4cf2
Isaque Rocha <isaquerocha@gmail.com>
2021-10-11 19:30:27 -0300

    test: adiciona configuração de testes, e um teste de exemplo

---
+++ src/test/java/com/wallet/HelloWorldTests.java
@@ -0,0 +1,16 @@
+package com.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class HelloWorldTests {
+
+    @Test
+    void testHelloWorld() {
+        assertTrue( true );
+    }
+}
