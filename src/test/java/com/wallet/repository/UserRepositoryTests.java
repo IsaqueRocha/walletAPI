fdf8ce672e8eab1b3649ac64e5f59a3ff02ee80c
Isaque Rocha <isaquerocha@gmail.com>
2021-10-12 09:34:41 -0300

    feat: adiciona busca por email no UserRepository e seu respectivo teste

--- src/test/java/com/wallet/repository/UserRepositoryTests.java
+++ src/test/java/com/wallet/repository/UserRepositoryTests.java
@@ -1,15 +1,36 @@
 package com.wallet.repository;
 
 import com.wallet.entity.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 class UserRepositoryTests {
+    private static final String EMAIL = "email@test.com";
+
     @Autowired
     UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User u = new User();
+        u.setName( "Setup User " );
+        u.setPassword( "senha123" );
+        u.setEmail( EMAIL );
+
+        userRepository.save( u );
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void testSave() {
@@ -21,5 +42,12 @@ class UserRepositoryTests {
         User response = userRepository.save( u );
 
         Assertions.assertNotNull( response );
+    }
+
+    @Test
+    void testFindByEmail() {
+        Optional<User> response = userRepository.findByEmailEquals( EMAIL );
+        Assertions.assertTrue( response.isPresent() );
+        Assertions.assertEquals( EMAIL, response.get().getEmail() );
     }
 }
